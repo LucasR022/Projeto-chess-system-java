7172351991967b36bba26e9c36bbda8f7f3beaf6
LucasR022 <lucas.roberto200@gmail.com>
2023-12-20 17:18:25 -0300

    feat: Moving Pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.println("Source: ");
+            ChessPosition source = UI.readChessPosition(scan);
+
+            System.out.println();
+            System.out.println("Target: ");
+            ChessPosition target = UI.readChessPosition(scan);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
